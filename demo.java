448f83f4d31da0be3cc82a880e22c938631e3376
ShaikhSaif <shaikhsaif82607@gmail.com>
2026-07-17 16:47:11 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("This Java loop no: " + i);
         }
     }
